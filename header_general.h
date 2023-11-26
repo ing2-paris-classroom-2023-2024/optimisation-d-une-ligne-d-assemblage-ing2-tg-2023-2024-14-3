b99fb65614a46209e3c643b5a2f3a17622ba014b
Romain Louazon <romain.louaz@gmail.com>
2023-11-26 21:55:33 +0100

    Try global 2

--- header_general.h
+++ header_general.h
@@ -115,7 +115,7 @@ typedef struct STATION{
 
 // Fonctions globales
 
-void DFS(DATASET dataset, TASK** selection, int nb_selection, TASK* tache, int MARQUEUR_PREC);
+//void DFS(DATASET dataset, TASK** selection, int nb_selection, TASK* tache, int MARQUEUR_PREC);
 // Procédure de parcours en DFS pour la séléction des tâches à effectuer.
 
 void ALGO(DATASET dataset); // Procédure générale du projet qui instancie et affiche les différentes stations.
