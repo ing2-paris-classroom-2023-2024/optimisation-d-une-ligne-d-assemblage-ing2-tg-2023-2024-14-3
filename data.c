f6b6eaaddc81ac3a2fa922b72532af44e66c1537
Marion BRAIDA <braidamarion@gmail.com>
2023-11-27 09:42:44 +0100

    debug + analyse données ajout succ

--- data.c
+++ data.c
@@ -130,7 +130,7 @@ DATASET DATASORT(DATAS datas){
         LIGNE[1]->P = (TASK**) realloc(LIGNE[1]->P, (LIGNE[1]->P_TOT+1)*sizeof(TASK*));
         LIGNE[1]->P[LIGNE[1]->P_TOT] = LIGNE[0];
         LIGNE[1]->P_TOT++;
-        printf("TACHE %d :\tAntecedent -> %d \t(PTOT %d)\n", LIGNE[1]->BASEID, LIGNE[1]->P[LIGNE[1]->P_TOT-1]->BASEID, LIGNE[1]->P_TOT);
+        //printf("TACHE %d :\tAntecedent -> %d \t(PTOT %d)\n", LIGNE[1]->BASEID, LIGNE[1]->P[LIGNE[1]->P_TOT-1]->BASEID, LIGNE[1]->P_TOT);
     }
 
     // Ajout des successeurs
@@ -152,7 +152,7 @@ DATASET DATASORT(DATAS datas){
         LIGNE[0]->S = (TASK**) realloc(LIGNE[0]->S, (LIGNE[0]->S_TOT+1)*sizeof(TASK*));
         LIGNE[0]->S[LIGNE[0]->S_TOT] = LIGNE[1];
         LIGNE[0]->S_TOT++;
-        //printf("TACHE %d :\tSuccesseur -> %d \t(PTOT %d)\n", LIGNE[0]->BASEID, LIGNE[0]->S[LIGNE[0]->S_TOT-1]->BASEID, LIGNE[0]->S_TOT);
+        printf("TACHE %d :\tSuccesseur -> %d \t(PTOT %d)\n", LIGNE[0]->BASEID, LIGNE[0]->S[LIGNE[0]->S_TOT-1]->BASEID, LIGNE[0]->S_TOT);
     }
 
     //Ajout des exclusions
