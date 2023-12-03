c2abcc232093dd4069c6cc74e457352dca7b949c
Paul Debarbieux <hapavela@gmail.com>
2023-12-03 15:12:34 +0100

    Commentaires #20

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include "header_general.h"
 
 int main() {
+    /*
+     * Fonction main qui exécute les différents sous programme
+     * La variable jeu_donnees permet de facilement passer d'un jeu de donnée à un autre
+     * */
     setbuf(stdout, 0);
     char* jeu_donnees = "01";
     DATAS datas = SCANDATAS(jeu_donnees);
@@ -10,6 +14,8 @@ int main() {
     DATASET dataset = DATASORT(datas);
     DISPDATASET(dataset);
 
+    // Pour la version alternative :
+    //ALGO2(dataset);
     ALGO(dataset);
 
     FREEDATASET(dataset);
